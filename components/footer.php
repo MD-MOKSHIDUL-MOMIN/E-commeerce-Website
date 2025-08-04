3229bc9d31a7607c3d0a8649df1aaa0fe0c81fc1
MD-MOKSHIDUL-MOMIN <139842805+MD-MOKSHIDUL-MOMIN@users.noreply.github.com>
2025-08-04 22:45:54 +0600

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,60 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i>Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +88 01563527312</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +880 2 12345678</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> momin@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Mymensingh,Bangladesh </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+   <!-- Font Awesome for credit card icon -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+<div style="font-family: Arial; padding: 50px;">
+    <h1><i class="fas fa-money-check-alt"></i> Payment Available</h1>
+    <div style="display: flex; gap: 55px; align-items: center;">
+        <!-- Credit Card Icon -->
+        <i class="fas fa-credit-card" style="font-size: 80px; color: #333; line-height: 1;"></i>
+        
+        <!-- Bkash Icon -->
+        <img src="https://download.logo.wine/logo/BKash/BKash-bKash-Logo.wine.png" alt="Bkash" style="height: 100px;">
+        
+        <!-- Nagad Icon -->
+        <img src="https://download.logo.wine/logo/Nagad/Nagad-Horizontal-Logo.wine.png" alt="Nagad" style="height: 100px;">
+        
+        <!-- Google Pay Icon -->
+        <img src="https://images.seeklogo.com/logo-png/33/1/google-pay-logo-png_seeklogo-334912.png" alt="Google Pay" style="height: 100px;">
+    </div>
+</div>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>M M Momin </span> | all rights reserved!</div>
+
+</footer>
